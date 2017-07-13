20db28c054e2adf2af023ae66a12b04684e46208
indexit-devops <raghu.indexit@gmail.com>
2017-07-13 09:05:50 +0530

    Update install_tomcat.sh

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $(id -u) != 0 ]; then 
+  echo "You should be a root user to perform this script"
+  exit 1
+fi
+
 echo "Installing TOmcat"
 yum install tomcat -y
 eho "Starting Tomcat"
